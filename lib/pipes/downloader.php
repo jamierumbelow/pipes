7db063c65e92df4e037d890a3ee9cffac2318097
Jamie Rumbelow <jamie@jamierumbelow.net>
2010-12-12 14:22:59 +0000

    Added some class file skeletons

---
+++ lib/pipes/downloader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Pipes
+ *
+ * Sexy PHP package management
+ *
+ * @package pipes
+ * @subpackage core
+ * @author Jamie Rumbelow <http://jamierumbelow.net>
+ * @copyright Copyright (c) 2010 Jamie Rumbelow
+ * @license MIT License
+ **/
+
+class Pipes_Downloader { }
---
+++ lib/pipes/vcs.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Pipes
+ *
+ * Sexy PHP package management
+ *
+ * @package pipes
+ * @subpackage core
+ * @author Jamie Rumbelow <http://jamierumbelow.net>
+ * @copyright Copyright (c) 2010 Jamie Rumbelow
+ * @license MIT License
+ **/
+
+class Pipes_Vcs { }
+
+class Pipes_Git extends Pipes_Vcs { }
+class Pipes_Hg extends Pipes_Vcs { }
+class Pipes_Svn extends Pipes_Vcs { }
