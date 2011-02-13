21f443095bca5b9e4ea9b049fa37baa14d965820
Alexander Baldwin <zanders@zafr.net>
2011-02-13 19:42:02 +0000

    Copy to-do list over to branch(diewindowsdie), progress will be made here until ready to merge back into master.

--- setup.php
+++ setup.php
@@ -17,6 +17,20 @@ require_once 'pipes/helpers.php';
 require_once 'pipes/constants.php';
 require_once 'pipes/version.php';
 
+/**
+
+	Windows Support To-do (Setup Only)
+	==================================
+	
+	- Create a BAT executable that calls "php <pathToPipesExecutable> arguments".
+	- Remove the "#/usr/bin/php" first line, else this will get send as output to the user.
+	- Save the batch file to a directory in the Windows system path variable.
+	-- Unfortunately, the only gaurantees are "C:\WINDOWS" or "C:\WINDOWS\system32", and I very much doubt PHP will have permissions to write there.
+	-- The alternative is to install to the PHP installation directory, though I currently do not know of any way to determine this via PHP.
+
+/**/
+
+
 // Get the contents of the binary
 $binary = file_get_contents('./bin/pipes');
 
